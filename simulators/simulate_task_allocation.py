e9afb9f2af63772425736a2457915a6af86f61d1
Hugo Sadok <sadok@gta.ufrj.br>
2017-05-05 09:09:12 -0300

    Using tqdm, stop user translation, 2x resources
    This replaces progress.Bar by tqdm, better performance and cleaner code.
    
    We were mapping users id to a counter twice: once in the
    simulate_task_allocation and another in the Task class itself. This made
    us loose the original id -- not anymore.
    
    Amount of total resources is now 2x the mean. This shoudn't matter as it
    will be replaced by cli parameter soon...

--- simulators/simulate_task_allocation.py
+++ simulators/simulate_task_allocation.py
@@ -3,7 +3,8 @@ from collections import defaultdict
 import threading
 import itertools
 import pandas as pd
-from progress.bar import Bar
+# from progress.bar import Bar
+from tqdm import tqdm
 
 from allocators.arrival import Task
 from tasks import tasks_file_header, save_task_deque
@@ -12,33 +13,29 @@ from helpers.schema import SchemaIndex
 
 
 def tasks_generator(tasks_df):
-    user_index_generator = itertools.count()
-    user_index = defaultdict(user_index_generator.next)
     task_index = SchemaIndex(tasks_file_header)
-    tasks_iterator = Bar('Processing %(eta_td)s', suffix='%(percent)d%%',
-                         max=len(tasks_df)).iter(tasks_df.itertuples())
-    for task_line in tasks_iterator:
+    for task_line in tqdm(tasks_df.itertuples(), total=len(tasks_df)):
         submit_time = task_line[task_index['submit_time']]
-        user = user_index[task_line[task_index['user_id']]]
+        user_index = task_line[task_index['user_id']]
         duration = task_line[task_index['finish_time']] - \
                    task_line[task_index['start_time']]
         demands = (task_line[task_index['cpu']],
                    task_line[task_index['memory']])
         task_id = task_line[task_index['task_id']]
-        task = Task(user, task_id, duration, demands, submit_time)
+        task = Task(user_index, task_id, duration, demands, submit_time)
         yield task
 
 
 def simulate_task_allocation(tasks_file, saving_file):
     tasks_df = pd.read_csv(tasks_file, header=None, index_col=False,
-                           names=tasks_file_header, nrows=100)  # TODO rm nrows
+                           names=tasks_file_header)
     print 'sorting...'
     tasks_df.sort_values('submit_time', inplace=True)
     print 'sorted...'
 
     # we may try to give 0 resources to all users at first
     #                  CPU    RAM    1000 users
-    allocator = WDRF([764.0, 673.0], [1]*1000)
+    allocator = WDRF([1528.0, 1346.0], [1]*1000)
     done = threading.Event()
     saving_thread = threading.Thread(target=save_task_deque, args=(
         allocator.finished_tasks, saving_file, done))
