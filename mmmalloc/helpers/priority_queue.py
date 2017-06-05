693b72038f77f8bc698a6126b2d340a8ee3cb06d
Hugo Sadok <sadok@gta.ufrj.br>
2017-06-05 12:52:56 -0300

    Improving the performance of the priority queue
    We leverage the SortedList container adding O(1) deletion for any
    element using a python dict

--- mmmalloc/helpers/priority_queue.py
+++ mmmalloc/helpers/priority_queue.py
@@ -1,62 +1,76 @@
 # -*- coding: utf-8 -*-
-import heapq
+from sortedcontainers import SortedList
 
 
 class PriorityQueue(object):
     def __init__(self, initial=None):
         self.finder = {}
         if initial is None:
-            self.heap = []
+            self.heap = SortedList()
         else:
-            self.heap = [[p, i] for i, p in enumerate(initial)]
+            self.heap = SortedList([p, i] for i, p in enumerate(initial))
             for i in self.heap:
                 self.finder[i[1]] = i
-            heapq.heapify(self.heap)
+        self.pending_removal = []
         self.removed_name = '__REMOVED__'
 
     def add(self, name, priority):
+        self.cleanup_pending_removal()
+
         if name in self.finder:
             if priority == self.finder[name][0]:
                 return
             self.remove(name)
         entry = [priority, name]
         self.finder[name] = entry
-        heapq.heappush(self.heap, entry)
+        self.heap.add(entry)
 
-    def pop(self):
+    def pop(self, get_priority=False):
+        self.cleanup_pending_removal()
+
         while self.heap:
-            priority, name = heapq.heappop(self.heap)
+            priority, name = self.heap.pop(0)
             if name != self.removed_name:
                 del self.finder[name]
+                if get_priority:
+                    return name, priority
                 return name
         return None
 
-    def get_min(self):
+    def get_min(self, get_priority=False):
+        self.cleanup_pending_removal()
+
         while self.heap:
             priority, name = self.heap[0]
             if name == self.removed_name:
-                heapq.heappop(self.heap)
+                del self.heap[0]
             else:
+                if get_priority:
+                    return name, priority
                 return name
         return None
 
-    def sorted_elements(self):
+    def sorted_elements(self, get_priority=False):
+        self.cleanup_pending_removal()
+
         def elements_iterator():
-            min_element = self.get_min()  # try not to sort at first
-
-            if min_element is None:
-                return
-
-            yield min_element
-
-            self.heap.sort()
-            for i in self.heap[1:]:
-                name = i[1]
+            for index, (priority, name) in enumerate(self.heap):
                 if name != self.removed_name:
-                    yield i
+                    if get_priority:
+                        yield name, priority
+                    else:
+                        yield name
+                else:
+                    self.pending_removal.append(index)
 
         return elements_iterator()
 
     def remove(self, name):
-        entry = self.finder[name]
+        self.cleanup_pending_removal()
+        entry = self.finder.pop(name)
         entry[1] = self.removed_name
+
+    def cleanup_pending_removal(self):
+        for index in reversed(self.pending_removal):
+            del self.heap[index]
+        self.pending_removal = []
