2841b3a61155364db1831d7e7a4b210f7ffc862e
Hugo Sadok <sadok@gta.ufrj.br>
2017-06-05 13:01:30 -0300

    Adding function to count number of lines in a file
    This is done in a fast way using a snippet from Stack Overflow

--- mmmalloc/helpers/__init__.py
+++ mmmalloc/helpers/__init__.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+import mmap
+
+
+def get_line_number(file_path):
+    fp = open(file_path, "r+")
+    buf = mmap.mmap(fp.fileno(), 0)
+    lines = 0
+    while buf.readline():
+        lines += 1
+    return lines
