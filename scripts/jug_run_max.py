c9dc17996bb8661e01cf10824fb0d7499a317b77
Hugo Sadok <sadok@gta.ufrj.br>
2017-06-06 09:07:43 -0300

    Adding delay between process launches
    There is a spike on memory usage right in the beginning the delay avoids
    that all tasks have this spike at once

--- scripts/jug_run_max.py
+++ scripts/jug_run_max.py
@@ -1,6 +1,7 @@
 import click
 from psutil import cpu_count
 from subprocess import call
+import time
 
 
 @click.command()
@@ -8,8 +9,9 @@ from subprocess import call
 def main(command):
     call(['tmux', 'new', '-s', 'credibility allocation', '-d'])
 
-    for _ in xrange(cpu_count()):
+    for _ in xrange(cpu_count()/2):
         call(['tmux', 'new-window', command])
+        time.sleep(60)
     # ' jug execute code/scripts/multicore_task.py data/filtered_tasks.csv'
 
 if __name__ == '__main__':
