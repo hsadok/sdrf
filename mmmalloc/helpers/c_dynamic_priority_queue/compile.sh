fd20966a90249a074a779b0eb7318ed7d831d19b
Hugo Sadok <hugombarreto@gmail.com>
2017-06-25 22:04:16 -0300

    Using a simpler command to compile
    This new command is also compatible with Clang

--- mmmalloc/helpers/c_dynamic_priority_queue/compile.sh
+++ mmmalloc/helpers/c_dynamic_priority_queue/compile.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-g++ -fdiagnostics-color=always --std=c++11 -O3 -Wall -Wextra -pedantic -c -fPIC c_priority_queue.cpp
-g++ -fdiagnostics-color=always --std=c++11 -O3 -Wall -Wextra -pedantic -shared -Wl,-soname,lib_c_priority_queue.so -o lib_c_priority_queue.so c_priority_queue.o
+g++ -fdiagnostics-color=always -shared -fPIC -O3 --std=c++11 -Wall -Wextra -pedantic -o lib_c_priority_queue.so c_priority_queue.cpp
